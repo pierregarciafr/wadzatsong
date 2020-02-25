7ec070bb6b9bc0bd68336aa3dbc1d3ee06c985fc
Estelle Saget <estelle.saget@live.fr>
2020-02-25 15:11:18 +0100

    def create games + button css

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,17 @@
 class GamesController < ApplicationController
 
-  def create
-    @game = policy_scope(Game.find(params[:id]))
-  end
+  # def new
+  #   @game = Game.new()
+  # end
 
+  # def create
+  #   # @game = policy_scope(Game.find(params[:id]))
+  #   @game = Game.new(games_params)
+  #   @game.user = current_user
+  #   if @game.save
+  #     redirect_to edit_game_path(@game)
+  #   else
+  #     render :new
+  #   end
+  # end
 end
