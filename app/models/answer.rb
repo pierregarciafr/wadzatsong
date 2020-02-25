c9243a797829dd9771abb83a758c09d704d7d670
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-02-25 10:54:49 +0100

    finish models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :game
   belongs_to :track
   belongs_to :user
+  validates :content, presence: true
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,8 @@ class Game < ApplicationRecord
   belongs_to :user
   belongs_to :playlist
   has_many :participations
+  has_many :participants, through: :participations, source: :users
   has_many :answers
+  has_many :tracks, through: :playlists
   enum status: [ :created, :running, :paused, :finished ]
 end
