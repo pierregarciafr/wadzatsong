ddad22628d2f38fe0ea25f6a91cb0588e3b54dba
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-02-27 17:45:51 +0100

    js

--- app/javascript/channels/game_channel.js
+++ app/javascript/channels/game_channel.js
@@ -2,11 +2,18 @@ import consumer from "./consumer";
 
 const answersContainer = document.getElementById('answers');
 
+const buzzerElt = document.getElementById('buzzer');
+const clickBuzzer = buzzerElt.addEventListener('click', (event) => {
+  window.location.reload();
+});)
+const answerElt = document.getElementById('answer');
+
 
 if (answersContainer) {
   const id = answersContainer.dataset.gameId; // .gameId ?
   consumer.subscriptions.create({ channel: "GameChannel", id: id }, {
     received(data) {
+
       window.location.reload();
 
       // answersContainer.insertAdjacentHTML('beforeend', data);
