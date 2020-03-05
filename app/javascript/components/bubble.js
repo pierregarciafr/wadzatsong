39d3f34bd6f625c95457155db8cc15cc0c1fefa2
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-03-05 12:50:24 +0100

    remove bubble again

--- app/javascript/components/bubble.js
+++ app/javascript/components/bubble.js
@@ -1,8 +1,9 @@
 
 const removeBubbleGood = () => {
+  console.log("coucou good");
   const bubble = document.querySelector(".speech-bubble-good-answer");
   if (bubble) {
-
+    console.log(bubble);
     setTimeout(() => {
       bubble.classList.add('zoomOut');
     }, 3000);
@@ -10,9 +11,10 @@ const removeBubbleGood = () => {
 };
 
 const removeBubbleBad = () => {
+  console.log("coucou bad");
   const bubble = document.querySelector(".speech-bubble-bad-answer");
   if (bubble) {
-
+    console.log(bubble);
     setTimeout(() => {
       bubble.classList.add('zoomOut');
     }, 3000);
