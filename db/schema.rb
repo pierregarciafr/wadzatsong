befe3fa57177e3d30d7c95f64fb5b6500afecbed
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-03-31 21:21:45 +0200

    integre le total_score du MJ comme une variable de @game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_133235) do
+ActiveRecord::Schema.define(version: 2020_03_31_174909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_133235) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "token"
+    t.integer "total_score"
     t.index ["playlist_id"], name: "index_games_on_playlist_id"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
