b5011f83b73019d97f02952f6104219edb695dd8
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-01 15:24:02 +0200

    create playlist job

---
+++ app/jobs/playlist_job.rb
@@ -0,0 +1,7 @@
+class PlaylistJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ test/jobs/playlist_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PlaylistJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
