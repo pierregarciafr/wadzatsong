d1850ed5f25e7879f407da90ff7f1c04eb538c62
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-04-06 08:16:05 +0200

    clean show users and refacto controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,24 +4,54 @@ class UsersController < ApplicationController
     authorize @user
     @faster_answer = Answer.where(status:true).order(answering_time: :ASC).first
     @user_faster_answer = @user.answers.where(status:true).order(answering_time: :ASC).first
-    @total_good_answers = @user.answers.where(status: true).count
-    total_tracks = get_total_tracks(@user)
-    @pourcent_user_good_answers = (@total_good_answers * 100) / total_tracks
+    @percent_user_good_answers = get_total_percent(@user)
+    @french_percent = get_playlist_percent(@user, "French")
+    @rock_percent = get_playlist_percent(@user, "Rock")
+    @disco_percent = get_playlist_percent(@user, "Disco")
+    @pop_percent = get_playlist_percent(@user, "Pop")
+    @rnb_percent = get_playlist_percent(@user, "R-N-B")
+    @electro_percent = get_playlist_percent(@user, "Electro")
   end
 
   private
 
-  def get_total_tracks(user)
-    total_tracks = 0
-    user.games.each do |game|
-      total_tracks += game.playlist.tracks.count
-    end
-    user.participations.each do |participation|
-      total_tracks += participation.game.playlist.tracks.count
-    end
-    return total_tracks
+  def total_tracks(user)
+    (user.games.count + user.participations.count) * Game::NUMBER_OF_ROUNDS
   end
 
+  def get_total_percent(user)
+    (user.answers.where(status: true).count * 100) / total_tracks(user)
+  end
 
+  def get_playlist_percent(user, playlist)
+    playlist_theme_array = []
+    Playlist.where(name:playlist).each do |playlist|
+      playlist_theme_array << playlist.id
+    end
 
+    count_games = 0
+    total_user_good_answers = 0
+
+    user.games.each do |game|
+      if playlist_theme_array.include?(game.playlist_id)
+        count_games += 1
+        good_answers = game.answers.where("status = ? AND user_id = ?", true, @user.id).count
+        total_user_good_answers += good_answers
+      end
+    end
+
+    user.participations.each do |participation|
+      if playlist_theme_array.include?(participation.game.playlist_id)
+        count_games += 1
+        good_answers = participation.game.answers.where("status = ? AND user_id = ?", true, @user.id).count
+        total_user_good_answers += good_answers
+      end
+    end
+    if (count_games * Game::NUMBER_OF_ROUNDS) != 0
+      result = (total_user_good_answers * 100) / (count_games * Game::NUMBER_OF_ROUNDS)
+    else
+    result = "-"
+    end
+    return result
+  end
 end
