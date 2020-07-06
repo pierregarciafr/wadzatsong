b82a84ad312bd1c1b7fa41936cf7742cb1309fb5
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-07-06 20:04:10 +0200

    complete user stat queries on dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,6 @@ class User < ApplicationRecord
   end
 
   def good_answers_percentage
-    # return '-' if games.count == 0 && participations.count == 0
     played_tracks != 0 ? (answers.where(status: true).count * 100) / played_tracks : '-'
   end
 
@@ -44,71 +43,21 @@ class User < ApplicationRecord
   end
 
   def percentages
-    # get_playlist_percent("French")
-    # { french_percent: get_playlist_percent("French"),
-    #   rock_percent: get_playlist_percent("Rock"),
-    #   dance_percent: get_playlist_percent("Dance"),
-    #   pop_percent: get_playlist_percent("Pop"),
-    #   rnb_percent: get_playlist_percent("R-N-B"),
-    #   electro_percent: get_playlist_percent("Electro")
-    # }
+    { french: success_percentage("French"),
+      rock: success_percentage("Rock"),
+      dance: success_percentage("Dance"),
+      pop: success_percentage("Pop"),
+      RNB: success_percentage("R-N-B"),
+      electro: success_percentage("Electro")
+    }
   end
 
   private
 
-  def get_playlist_percent(genre, user)
-    good_answers_per_genre = Playlist.where(name:genre).joins(tracks: :answers).where(answers:{status:true, user_id: user.id}).count
+  def success_percentage(genre)
+    good_answers_per_genre = Playlist.where(name:genre).joins(tracks: :answers).where(answers:{status:true, user_id: self.id}).count
     total_tracks = Game.where(playlist_id:Playlist.find_by(name:genre)).joins(playlist: :tracks).count
-    print "#{good_answers_per_genre} / #{total_tracks} "
-    total_tracks ? good_answers_per_genre.fdiv(total_tracks) : 'Division par 0!'
+    total_tracks != 0 ? "#{(good_answers_per_genre*100.fdiv(total_tracks)).round}%" : '-'
   end
-
-    # 21 06 20
-    # nombre total de tracks jouées pour un genre donné
-
-    # total_good_answers(user) = Answer.where(user_id:user.id)
-                                     # .where(status:true)
-                                     # .joins(track: :playlist).count
-# Answer.where(user_id:user.id).where(status:true).joins(track: :playlist).count
-# Track.find_by_sql('SELECT * FROM tracks t JOIN answers a ON a.id = t.answer_id JOIN playlist p ON p.id = t.playlist_id WHERE a.status = true AND p.name = ?', 'French');
-
-
-
-    # good_answers_per_genre = Playlist.where(name:genre).joins(tracks: :answers).where(answers:{status: true,user_id: self}).count
-    # good_answers_per_genre = Track.joins(:answers).where(answers:{status: true, user_id: self}).joins(:playlist).where(playlist:{name: genre})
-    #good_answers_per_genre = Answer.joins(:track, :playlist).where(answers:{status: true, user_id: self},playlist:{name: genre})
-
-
-    # tracks_per_genre = Playlist.where(name:genre).joins(:tracks).count
-    # (good_answers_per_genre.fdiv(tracks_per_genre)).round
-
-
-
-    # count_games = 0
-    # total_user_good_answers = 0
-
-    # user.games.each do |game|
-    #   if playlist_theme_array.include?(game.playlist_id)
-    #     count_games += 1
-    #     good_answers = game.answers.where("status = ? AND user_id = ?", true, @user.id).count
-    #     total_user_good_answers += good_answers
-    #   end
-    # end
-
-    # user.participations.each do |participation|
-    #   if playlist_theme_array.include?(participation.game.playlist_id)
-    #     count_games += 1
-    #     good_answers = participation.game.answers.where("status = ? AND user_id = ?", true, @user.id).count
-    #     total_user_good_answers += good_answers
-    #   end
-    # end
-    # if (count_games * Game::NUMBER_OF_ROUNDS) != 0
-    #   result = (total_user_good_answers * 100) / (count_games * Game::NUMBER_OF_ROUNDS)
-    # else
-    # result = "-"
-    # end
-    # return result
-  # end
-
 
 end
